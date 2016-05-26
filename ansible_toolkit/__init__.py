03023cbd3c7047937ebcfdb43d98de7078f65e55
Pedro Salgado <steenzout@saucelabs.com>
2016-05-26 15:23:15 -0600

    changed ansible_toolkit.show_variables() signature and return value

--- ansible_toolkit/__init__.py
+++ ansible_toolkit/__init__.py
@@ -21,14 +21,14 @@ def get_vault(vault_password_file):
     return dao.get_vault(vault_password_file)
 
 
-def show_variables(host, inventory, vault_password_file):
+def show_variables(host, inventory_path=None, vault_password_file=None):
     """
 
     :param host: the host name for which you want to display variables.
-    :param inventory: the inventory.
+    :param inventory_path: the path to the Ansible inventory.
     :param vault_password_file:
         the path to the Ansible vault password file.
     """
     dao = create_dao()
-    dao.show_variables(
-        host, inventory, vault_password_file)
+    return dao.show_variables(
+        host, inventory_path, vault_password_file)
