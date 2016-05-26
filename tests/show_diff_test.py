8fc889eb38d736892455520cdd17f68db1798fac
Pedro Salgado <steenzout@saucelabs.com>
2016-05-26 09:44:42 -0600

    added test for show_diff with no changes

---
+++ tests/show_diff_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import mock
+import unittest
+
+from ansible_toolkit.show_vars import show_diff
+
+
+class TestShowDiff(unittest.TestCase):
+
+    """Test case for the ansible_toolkit.show_vars module."""
+
+    @mock.patch('ansible_toolkit.utils.sys.stdout.write', autospec=True)
+    def test_no_diff(self, mock_write):
+
+        old = {}
+        new = {}
+
+        self.assertIsNone(show_diff(old, new))
+        self.assertFalse(mock_write.called)
