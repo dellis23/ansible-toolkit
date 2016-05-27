ee03a9340d4f751e20d4046aba5c16c1f781e7a8
Pedro Salgado <steenzout@saucelabs.com>
2016-05-26 21:29:16 -0600

    added test case to verify ansible_toolkit.dao.AnsibleDao interface.

--- tests/dao_test.py
+++ tests/dao_test.py
@@ -2,7 +2,10 @@
 
 import ansible
 import ansible_toolkit.dao
+import pytest
 import unittest
+
+from ansible_toolkit.dao import AnsibleDao
 
 
 class InitTestCase(unittest.TestCase):
@@ -15,6 +18,28 @@ class InitTestCase(unittest.TestCase):
     def test(self):
         dao = ansible_toolkit.dao.create_dao()
         self.assertEqual(ansible.__version__, dao.version)
+
+
+class InterfaceTestCase(unittest.TestCase):
+
+    """
+    Test case to verify ansible_toolkit.dao.AnsibleDao
+    interface contract that needs to be implemented by
+    its implementations.
+    """
+
+    def test_interface(self):
+        """Test functions to be implemented."""
+        interface = AnsibleDao()
+
+        with pytest.raises(NotImplementedError):
+            interface.get_vault(None)
+
+        with pytest.raises(NotImplementedError):
+            interface.get_vault_password(None)
+
+        with pytest.raises(NotImplementedError):
+            interface.show_variables(None)
 
 
 class ShowVariablesTestCase(unittest.TestCase):
