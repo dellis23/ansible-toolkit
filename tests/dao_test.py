466eefd14f5a4d82e3c786dada766fe759751dda
Pedro Salgado <steenzout@saucelabs.com>
2016-05-26 21:25:02 -0600

    added test case for ansible_toolkit.dao.AnsibleDao.__init__() function.

--- tests/dao_test.py
+++ tests/dao_test.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import ansible
 import ansible_toolkit.dao
 import unittest
 
 
+class InitTestCase(unittest.TestCase):
+
+    """
+    Test case for the
+    ansible_toolkit.dao.AnsibleDao.__init__() function.
+    """
+
+    def test(self):
+        dao = ansible_toolkit.dao.create_dao()
+        self.assertEqual(ansible.__version__, dao.version)
+
+
 class ShowVariablesTestCase(unittest.TestCase):
 
-    """Test case for the ansible_toolkit.dao.show_variables() function."""
+    """
+    Test case for the
+    ansible_toolkit.dao.AnsibleDao.show_variables() function.
+    """
 
     def setUp(self):
         self.dao = ansible_toolkit.dao.create_dao()
